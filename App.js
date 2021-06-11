71884bcc1ad5106b94db25823904924e47924e9e
swaaz <swaasthik.shetty07@gmail.com>
2021-06-11 17:54:21 +0530

    added patient and doctor pages

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import DoctorInitial from './screens/DoctorInitial';
 import LandingOption from './screens/LandingOption';
 import LandingPage from './screens/LandingPage';
+import PatientInitial from './screens/PatientInitial';
 
 export default function App() {
   return (
-    <LandingOption />
+    <PatientInitial />
   );
 }
 
