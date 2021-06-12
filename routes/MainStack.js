199bf5958c6dfa5913a0e1c31467d44f5dffe006
suhanacharya <suhan.acharya1@gmail.com>
2021-06-12 18:59:15 +0530

    Add routes

--- routes/MainStack.js
+++ routes/MainStack.js
@@ -6,47 +6,77 @@ import DoctorInitial from '../screens/DoctorInitial';
 import PatientInitial from '../screens/PatientInitial';
 import DoctorSignUp from '../screens/DoctorSignUp';
 import PatientSignUp from '../screens/PatientSignUp';
+import AddHealthIssuesPage from '../screens/AddHealthIssuesPage'
+import AddHospitalizationPage from '../screens/AddHospitalizationPage'
+import AddVaccinePage from '../screens/AddVaccinePage'
+import AddSurgeryPage from '../screens/AddSurgeryPage'
 
 
 const screens = {
-    PatientSignUp : {
-        screen : PatientSignUp,
-        navigationOptions : {
-            headerShown : false
-        }
-    },
-    DoctorSignUp : {
-        screen : DoctorSignUp,
-        navigationOptions : {
-            headerShown : false
-        }
-    },
-    DoctorInitial : {
-        screen: DoctorInitial,
-        navigationOptions : {
-            headerShown : false
-        }
-    },
-    LandingPage : {
-        screen : LandingPage,
-        navigationOptions : {
-            headerShown : false
-        }
-    },
-    LandingOption : {
-        screen : LandingOption,
-        navigationOptions : {
-            headerShown : false
-        }
-    },
-    PatientInitial : {
-        screen : PatientInitial,
-        navigationOptions : {
-            headerShown : false
-        }
-    },
-    
-    
+	// PatientSignUp: {
+	//     screen: PatientSignUp,
+	//     navigationOptions: {
+	//         headerShown: false
+	//     }
+	// },
+	// DoctorSignUp: {
+	//     screen: DoctorSignUp,
+	//     navigationOptions: {
+	//         headerShown: false
+	//     }
+	// },
+	// DoctorInitial: {
+	//     screen: DoctorInitial,
+	//     navigationOptions: {
+	//         headerShown: false
+	//     }
+	// },
+	// LandingPage: {
+	//     screen: LandingPage,
+	//     navigationOptions: {
+	//         headerShown: false
+	//     }
+	// },
+	// LandingOption: {
+	//     screen: LandingOption,
+	//     navigationOptions: {
+	//         headerShown: false
+	//     }
+	// },
+	// PatientInitial: {
+	//     screen: PatientInitial,
+	//     navigationOptions: {
+	//         headerShown: false
+	//     }
+	// },
+	// AddHealthIssuesPage: {
+	// 	screen: AddHealthIssuesPage,
+	// 	navigationOptions: {
+	// 		headerShown: false
+	// 	}
+	// },
+	// AddHospitalizationPage: {
+	// 	screen: AddHospitalizationPage,
+	// 	navigationOptions: {
+	// 		headerShown: false
+	// 	}
+	// },
+	AddVaccinePage: {
+		screen: AddVaccinePage,
+		navigationOptions: {
+			headerShown: false
+		}
+	},
+	AddSurgeryPage: {
+		screen: AddSurgeryPage,
+		navigationOptions: {
+			headerShown: false
+		}
+	},
+
+
+
+
 }
 
 const AuthStack = createStackNavigator(screens);
